67ffdd0ac5066ecfb490dd9c592eb802d303c3ed
Alex Still <alexstill@hotmail.co.uk>
2026-04-28 21:32:20 +0100

    Initial commit

---
+++ src/test/java/constants/Constants.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class Constants {
+    public static boolean testBool = false;
+    public static String testString = "Testing...";
+    public static int testInt = 0;
+    public static double testDouble = 1.2;
+}
---
+++ src/test/java/stepdefs/LoginSteps.java
@@ -0,0 +1,27 @@
+package stepdefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import constants.Constants;
+
+public class LoginSteps {
+
+    public String message = "This is a TEST";
+
+    @Given("user is on login page")
+    public void user_is_on_login_page() {
+        System.out.println("On login page");
+    }
+
+    @When("user enters valid username and password")
+    public void user_enters_valid_username_and_password() {
+        System.out.println("Entering credentials");
+    }
+
+    @Then("user should be logged in")
+    public void user_should_be_logged_in() {
+        System.out.println("Logged in as: " + message);
+        System.out.println(Constants.testString);
+    }
+}
