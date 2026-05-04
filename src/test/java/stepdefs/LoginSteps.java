d15953265e7d9e539e5445143bba5ac07080fc36
Alex Still <alexstill@hotmail.co.uk>
2026-05-05 00:08:54 +0100

    Login steps extension

--- src/test/java/stepdefs/LoginSteps.java
+++ src/test/java/stepdefs/LoginSteps.java
@@ -19,9 +19,20 @@ public class LoginSteps {
         System.out.println("Entering credentials");
     }
 
+    @When("user enter invalid username and password")
+    public void user_enter_invalid_username_and_password() {
+        System.out.println("Entering invalid credentials");
+        System.out.println("INAVLID CREDENTIALS -- Please enter correct username and password");
+    }
+
     @Then("user should be logged in")
     public void user_should_be_logged_in() {
         System.out.println("Logged in as: " + message);
         System.out.println(Constants.testString);
+    }
+
+    @Then("User will not be logged in")
+    public void user_will_not_be_logged_in() {
+        System.out.println("User is NOT be logged in");
     }
 }
--- src/test/resources/features/test.feature
+++ src/test/resources/features/test.feature
@@ -4,3 +4,8 @@ Feature: Login feature
     Given user is on login page
     When user enters valid username and password
     Then user should be logged in
+
+  Scenario: Invalid login
+    Given user is on login page
+    When user enter invalid username and password
+    Then User will not be logged in
