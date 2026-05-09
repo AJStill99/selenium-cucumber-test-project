43a204091f7ff2ac5a4a6525854348d89fb2e1a9
Alex Still <alexstill@hotmail.co.uk>
2026-05-09 22:11:04 +0100

    Adding to main

---
+++ src/test/java/pages/Products.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+
+public class Products {
+    WebDriver driver;
+
+    // Define constants
+    private By
+
+    public Products(WebDriver driver) {
+        this.driver = driver;
+    }
+    // Call this function in the ProductSteps file, taking in DriverManager as an argument to give a driver
+
+
+}
